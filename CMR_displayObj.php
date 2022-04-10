339fe65104e800634cccb46be84f928d3c53dd80
Đào Minh Quyền <daoquyenminh@gmail.com>
2022-04-11 00:10:36 +0700

    add

---
+++ CMR_displayObj.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Hiển thị dân cư</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css" />
+    <script src="https://openlayers.org/en/v4.6.5/build/ol.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript"></script>
+    <style>
+        .map,
+        .righ-panel {
+            height: 98vh;
+            width: 80vw;
+            float: left;
+        }
+
+        .map {
+            border: 1px solid #000;
+            
+        }
+        
+    </style>
+</head>
+
+<body onload="initialize_map();">
+    <table>
+        <tr>
+            <td>
+                <div id="map" class="map"></div>
+                <!--<div id="map" style="width: 80vw; height: 100vh;"></div>-->
+            </td>
+            <td>
+                <div id="info"></div>
+               <a href="http://localhost/btl/">All file</a>
+            </td>
+        </tr>
+    </table>
+    <?php include 'CMR_pgsqlAPI.php' ?>
+    <script>
+        //$("#document").ready(function () {
+        var format = 'image/png';
+        var map;
+        var minX = -77.120849609375;
+        var minY = 38.7906188964844;
+        var maxX = -76.9080963134766;
+        var maxY = 38.9969940185547;
+        var cenX = (minX + maxX) / 2;
+        var cenY = (minY + maxY) / 2;
+        var mapLat = cenY;
+        var mapLng = cenX;
+        var mapDefaultZoom = 12;
+
+        function initialize_map() {
+            //*
+            layerBG = new ol.layer.Tile({
+                source: new ol.source.OSM({})
+            });
+            //*/
+            var layerCMR_adm1 = new ol.layer.Image({
+                source: new ol.source.ImageWMS({
+                    ratio: 1,
+                    url: 'http://localhost:8080/geoserver/btl/wms?',
+                    params: {
+                        'FORMAT': format,
+                        'VERSION': '1.1.0',
+                        STYLES: '',
+                        LAYERS: 'wards_from_2012',
+                    }
+                })
+            });
+            var viewMap = new ol.View({
+                center: ol.proj.fromLonLat([mapLng, mapLat]),
+                zoom: mapDefaultZoom
+                //projection: projection
+            });
+            map = new ol.Map({
+                target: "map",
+                layers: [layerBG, layerCMR_adm1],
+                //layers: [layerCMR_adm1],
+                view: viewMap
+            });
+            //map.getView().fit(bounds, map.getSize());
+
+            var styles = {
+                'MultiPolygon': new ol.style.Style({
+                    fill: new ol.style.Fill({
+                        color: 'orange'
+                    }),
+                    stroke: new ol.style.Stroke({
+                        color: 'red', // gốc là yellow
+                        width: 2
+                    })
+                })
+            };
+            var styleFunction = function(feature) {
+                return styles[feature.getGeometry().getType()];
+            };
+            var vectorLayer = new ol.layer.Vector({
+                //source: vectorSource,
+                style: styleFunction
+            });
+            map.addLayer(vectorLayer);
+
+            function createJsonObj(result) {
+                var geojsonObject = '{' +
+                    '"type": "FeatureCollection",' +
+                    '"crs": {' +
+                    '"type": "name",' +
+                    '"properties": {' +
+                    '"name": "EPSG:4326"' +
+                    '}' +
+                    '},' +
+                    '"features": [{' +
+                    '"type": "Feature",' +
+                    '"geometry": ' + result +
+                    '}]' +
+                    '}';
+                return geojsonObject;
+            }
+
+            function drawGeoJsonObj(paObjJson) {
+                var vectorSource = new ol.source.Vector({
+                    features: (new ol.format.GeoJSON()).readFeatures(paObjJson, {
+                        dataProjection: 'EPSG:4326',
+                        featureProjection: 'EPSG:3857'
+                    })
+                });
+                var vectorLayer = new ol.layer.Vector({
+                    source: vectorSource
+                });
+                map.addLayer(vectorLayer);
+            }
+
+            function displayObjInfo(result, coordinate) {
+                //alert("result: " + result);
+                //alert("coordinate des: " + coordinate);
+                $("#info").html(result);
+            }
+            map.on('singleclick', function(evt) {
+                //alert("coordinate org: " + evt.coordinate);
+                //var myPoint = 'POINT(12,5)';
+                var lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+                var lon = lonlat[0];
+                var lat = lonlat[1];
+                var myPoint = 'POINT(' + lon + ' ' + lat + ')';
+                //alert("myPoint: " + myPoint);
+                //*
+                $.ajax({
+                    type: "POST",
+                    url: "CMR_pgsqlAPI.php",
+                    //dataType: 'json',
+                    //data: {functionname: 'reponseGeoToAjax', paPoint: myPoint},
+                    data: {
+                        functionname: 'getInfoCMRToAjax',
+                        paPoint: myPoint
+                    },
+                    success: function(result, status, erro) {
+                        displayObjInfo(result, evt.coordinate);
+                    },
+                    error: function(req, status, error) {
+                        alert(req + " " + status + " " + error);
+                    }
+                });
+                //*/
+            });
+            function highLightGeoJsonObj(paObjJson) {
+                var vectorSource = new ol.source.Vector({
+                    features: (new ol.format.GeoJSON()).readFeatures(paObjJson, {
+                        dataProjection: 'EPSG:4326',
+                        featureProjection: 'EPSG:3857'
+                    })
+                });
+                vectorLayer.setSource(vectorSource);
+                /*
+                var vectorLayer = new ol.layer.Vector({
+                    source: vectorSource
+                });
+                map.addLayer(vectorLayer);
+                */
+            }
+
+            function highLightObj(result) {
+                //alert("result: " + result);
+                var strObjJson = createJsonObj(result);
+                //alert(strObjJson);
+                var objJson = JSON.parse(strObjJson);
+                //alert(JSON.stringify(objJson));
+                //drawGeoJsonObj(objJson);
+                highLightGeoJsonObj(objJson);
+            }
+            map.on('singleclick', function(evt) {
+                //alert("coordinate: " + evt.coordinate);
+                //var myPoint = 'POINT(12,5)';
+                var lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+                var lon = lonlat[0];
+                var lat = lonlat[1];
+                var myPoint = 'POINT(' + lon + ' ' + lat + ')';
+                //alert("myPoint: " + myPoint);
+                //*
+                $.ajax({
+                    type: "POST",
+                    url: "CMR_pgsqlAPI.php",
+                    //dataType: 'json',
+                    data: {
+                        functionname: 'getGeoCMRToAjax',
+                        paPoint: myPoint
+                    },
+                    success: function(result, status, erro) {
+                        highLightObj(result);
+                    },
+                    error: function(req, status, error) {
+                        alert(req + " " + status + " " + error);
+                    }
+                });
+                //*/
+            });
+        };
+        //});
+    </script>
+</body>
+
+</html>
---
+++ CMR_pgsqlAPI.php
@@ -0,0 +1,200 @@
+<?php
+    if(isset($_POST['functionname']))
+    {
+        $paPDO = initDB();
+        $paSRID = '4326';
+        $paPoint = $_POST['paPoint'];
+        $functionname = $_POST['functionname'];
+        
+        $aResult = "null";
+        if ($functionname == 'getGeoCMRToAjax')
+            $aResult = getGeoCMRToAjax($paPDO, $paSRID, $paPoint);
+        else if ($functionname == 'getInfoCMRToAjax')
+            $aResult = getInfoCMRToAjax($paPDO, $paSRID, $paPoint);
+        
+        echo $aResult;
+    
+        closeDB($paPDO);
+    }
+
+    function initDB()
+    {
+        // Kết nối CSDL
+        $paPDO = new PDO('pgsql:host=localhost;dbname=btl;port=5432','postgres','1');
+        return $paPDO;
+    }
+    function query($paPDO, $paSQLStr)
+    {
+        try
+        {
+            // Khai báo exception
+            $paPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            // Sử đụng Prepare 
+            $stmt = $paPDO->prepare($paSQLStr);
+            // Thực thi câu truy vấn
+            $stmt->execute();
+            
+            // Khai báo fetch kiểu mảng kết hợp
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            
+            // Lấy danh sách kết quả
+            $paResult = $stmt->fetchAll();   
+            return $paResult;                 
+        }
+        catch(PDOException $e) {
+            echo "Thất bại, Lỗi: " . $e->getMessage();
+            return null;
+        }       
+    }
+    function closeDB($paPDO)
+    {
+        // Ngắt kết nối
+        $paPDO = null;
+    }
+    function example1($paPDO)
+    {
+        $mySQLStr = "SELECT * FROM \"wards_from_2012\"";
+        $result = query($paPDO, $mySQLStr);
+
+        if ($result != null)
+        {
+            // Lặp kết quả
+            foreach ($result as $item){
+                echo $item['name_0'] . ' - '. $item['name_1'];
+                echo "<br>";
+            }
+        }
+        else
+        {
+            echo "example1 - null";
+            echo "<br>";
+        }
+    }
+    function example2($paPDO)
+    {
+        $mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"wards_from_2012\"";
+        $result = query($paPDO, $mySQLStr);
+        
+        if ($result != null)
+        {
+            // Lặp kết quả
+            foreach ($result as $item){
+                echo $item['geo'];
+                echo "<br><br>";
+            }
+        }
+        else
+        {
+            echo "example2 - null";
+            echo "<br>";
+        }
+    }
+    function example3($paPDO,$paSRID,$paPoint)
+    {
+        echo $paPoint;
+        echo "<br>";
+        $paPoint = str_replace(',', ' ', $paPoint);
+        echo $paPoint;
+        echo "<br>";
+        //$mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"CMR_adm1\" where ST_Within('SRID=4326;POINT(12 5)'::geometry,geom)";
+        $mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"wards_from_2012\" where ST_Within('SRID=".$paSRID.";".$paPoint."'::geometry,geom)";
+        echo $mySQLStr;
+        echo "<br><br>";
+        $result = query($paPDO, $mySQLStr);
+        
+        if ($result != null)
+        {
+            // Lặp kết quả
+            foreach ($result as $item){
+                echo $item['geo'];
+                echo "<br><br>";
+            }
+        }
+        else
+        {
+            echo "example2 - null";
+            echo "<br>";
+        }
+    }
+    function getResult($paPDO,$paSRID,$paPoint)
+    {
+        //echo $paPoint;
+        //echo "<br>";
+        $paPoint = str_replace(',', ' ', $paPoint);
+        //echo $paPoint;
+        //echo "<br>";
+        //$mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"CMR_adm1\" where ST_Within('SRID=4326;POINT(12 5)'::geometry,geom)";
+        $mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"wards_from_2012\" where ST_Within('SRID=".$paSRID.";".$paPoint."'::geometry,geom)";
+        //echo $mySQLStr;
+        //echo "<br><br>";
+        $result = query($paPDO, $mySQLStr);
+        
+        if ($result != null)
+        {
+            // Lặp kết quả 
+            foreach ($result as $item){
+                return $item['geo'];
+            }
+        }
+        else
+            return "null";
+    }
+    function getGeoCMRToAjax($paPDO,$paSRID,$paPoint)
+    {
+        //echo $paPoint;
+        //echo "<br>";
+        $paPoint = str_replace(',', ' ', $paPoint);
+        //echo $paPoint;
+        //echo "<br>";
+        //$mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"census2010\" where ST_Within('SRID=4326;POINT(12 5)'::geometry,geom)";
+        $mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"wards_from_2012\" where ST_Within('SRID=".$paSRID.";".$paPoint."'::geometry,geom)";
+        //echo $mySQLStr;
+        //echo "<br><br>";
+        $result = query($paPDO, $mySQLStr);
+        
+        if ($result != null)
+        {
+            // Lặp kết quả
+            foreach ($result as $item){
+                return $item['geo'];
+            }
+        }
+        else
+            return "null";
+    }
+    function getInfoCMRToAjax($paPDO,$paSRID,$paPoint)
+    {
+        //echo $paPoint;
+        //echo "<br>";
+        $paPoint = str_replace(',', ' ', $paPoint);
+        //echo $paPoint;
+        //echo "<br>";
+        //$mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"CMR_adm1\" where ST_Within('SRID=4326;POINT(12 5)'::geometry,geom)";
+        //$mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"CMR_adm1\" where ST_Within('SRID=".$paSRID.";".$paPoint."'::geometry,geom)";
+        $mySQLStr = "SELECT rep_name,rep_phone, pop_male, pop_female,  areasqmi, pop_2011_2 from \"wards_from_2012\" where ST_Within('SRID=".$paSRID.";".$paPoint."'::geometry,geom)";
+        //echo $mySQLStr;
+        //echo "<br><br>";
+        $result = query($paPDO, $mySQLStr);
+        
+        if ($result != null)
+        {
+            $resFin = '<table><thread>';
+            // Lặp kết quả
+            foreach ($result as $item){
+                
+                $resFin = $resFin.'<tr><td><b>Tên Phường:</b> '.$item['rep_name'].'</td></tr>';
+                $resFin = $resFin.'<tr><td><b>Diện tích</b> '.$item['areasqmi'].' Mét vuông</td></tr>';
+                $resFin = $resFin.'<tr><td><b>Dân số:</b> '.$item['pop_2011_2'].'</td></tr>';
+                $resFin = $resFin.'<tr><td><b>Số điện thoại:'.$item['rep_phone'].'</b></td></tr>';
+                $resFin = $resFin.'<tr><td><b>Nam giới:</b> '.$item['pop_male'].'</td></tr>';
+                $resFin = $resFin.'<tr><td><b>Nữ giới:</b> '.$item['pop_female'].'</td></tr>';
+                break;
+            }
+            $resFin = $resFin.'</table>';
+            return $resFin;
+        }
+        else
+            return "null";
+    }
+?>
